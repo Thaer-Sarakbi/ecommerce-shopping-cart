dd7c5cdb1605aaa521af9ab64b427844810078b0
Thaer Sarakbi <thaersarakbi@gmail.com>
2020-05-15 16:30:50 +0200

    Updates

---
+++ precache-manifest.fd1b2eaf441e3c286564b76f3d5fb068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bfa5ed7bd81d58dfb7b",
+    "url": "/ecommerce-shopping-cart/static/css/main.dab79894.chunk.css"
+  },
+  {
+    "revision": "9bfa5ed7bd81d58dfb7b",
+    "url": "/ecommerce-shopping-cart/static/js/main.5b6f722c.chunk.js"
+  },
+  {
+    "revision": "a0e27bc38c15e076523a",
+    "url": "/ecommerce-shopping-cart/static/js/runtime~main.e9daa8c9.js"
+  },
+  {
+    "revision": "8120163d6d3b91971b22",
+    "url": "/ecommerce-shopping-cart/static/js/2.c0796c35.chunk.js"
+  },
+  {
+    "revision": "a18218e1fb59a9a95a76d970283b5d75",
+    "url": "/ecommerce-shopping-cart/index.html"
+  }
+];
